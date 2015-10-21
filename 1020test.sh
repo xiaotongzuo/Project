a3501e95e53397c2370d2765f186d874e6e23bc1
xiaotongzuo <xiaotongzuo@gmail.com>
2015-10-20 21:13:01 -0400

    arrange folders

---
+++ 1020test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source ~/Pyrosetta/SetPyRosettaEnvironment.sh
+cd /home/Xiaotong/Github/Project/Code
+./1020test.py  
--- Code/1020test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source ~/Pyrosetta/SetPyRosettaEnvironment.sh
-cd /home/Xiaotong/Github/Project/Code
-./1020test.py  
